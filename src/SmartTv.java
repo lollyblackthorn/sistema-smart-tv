2f29412084d9c5ea129486c59e8f09a499c7d8c3
Lolly Blackthorn <lorranysantos08@gmail.com>
2023-09-22 14:09:11 -0300

    Exercício de classes e métodos.

---
+++ src/SmartTv.java
@@ -0,0 +1,36 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("Canal: " + canal);
+    }
+
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("Canal: " + canal);
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume: " + volume);
+    }
+
+    public void ligar(){//método
+        ligada = true;
+    }
+
+    public void desligar(){//método
+        ligada = false;
+    }
+}
